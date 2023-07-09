fa422081ea66182d87165743c62dfc04f4febae0
Prakash Poudel <prakaas,poudell@gmail.com>
2023-07-09 09:42:43 +0545

    linked main portfolio and unnecessary code

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
 <header id="header">
-    <h1><a href="/"><strong>Memories</strong> Prakash Poudel </a></h1>
+    <h1><strong>Memories</strong><a href="http://prakashpoudel.info.np" target="_blank"> Prakash Poudel </a></h1>
     <nav>
         <ul>
             <li><a href="#footer" class="icon solid fa-info-circle">About</a></li>
